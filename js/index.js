d2461355ed98651e95ad408bc8368478304db5d0
SeriousDim <dlykov2001@yandex.ru>
2022-09-01 22:12:53 +0500

    Refactoring js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+const ERROR_MESSAGE_CLASS = "error-message";
+const SUCCESS_MESSAGE_CLASS = "success-message";
+
+const ERROR_MESSAGE_STRING = "Не удалось отправить данные";
+const SUCCESS_MESSAGE_STRING = "Данные успешно отправлены";
+
+window.onload = function() {
+    var form = document.getElementById("modal-form");
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        sendData(e.target);
+    });
+};
+
 document.addEventListener("keyup", (e) => {
     var modal = document.getElementById("modal");
     if (e.code === "Escape") {
@@ -6,57 +21,60 @@ document.addEventListener("keyup", (e) => {
     }
 });
 
-window.onload = function() {
-    var form = document.getElementById("modal-form");
+function sendData(targetForm) {
+    var url = targetForm.action;
+    var data = new FormData(targetForm);
+    var dict = {};
 
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        var f = e.target;
-        var url = f.action;
-        var data = new FormData(f);
-        var dict = {};
+    for (const [name,value] of data) {
+        dict[name] = value;
+    }
+    console.log(dict);
 
-        for (const [name,value] of data) {
-            dict[name] = value;
-        }
-        console.log(dict);
+    fetch(url, {
+        method: "post",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
 
-        fetch(url, {
-            method: "post",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-
-            body: JSON.stringify(dict)
-        })
-        .then( (response) => { 
-            showMessage("Данные успешно отправлены", "success-message");
+        body: JSON.stringify(dict)
+    })
+    .then(
+        (response) => { 
+            showMessage(SUCCESS_MESSAGE_STRING, SUCCESS_MESSAGE_CLASS);
             console.log(response.json());
         },
-            (error) => {
-                showMessage("Не удалось отправить данные", "error-message");
+        (error) => {
+                showMessage(ERROR_MESSAGE_STRING, ERROR_MESSAGE_CLASS);
                 console.log(error);
-            });
-    });
-};
+        }
+    );    
+}
+
+function getMessage(modal) {
+    return modal.getElementsByClassName("message")[0];
+}
 
 function showMessage(text, colorClass) {
-    var mes = modal.getElementsByClassName("message")[0];
+    var modal = document.getElementById("modal");
+    var mes = getMessage(modal);
     mes.style.visibility = "visible";
 
-    mes.classList.remove("error-message");
-    mes.classList.remove("success-message");
+    resetMessageStyle(mes);
     mes.classList.add(colorClass);
-
     mes.innerHTML = text;
 }
 
 function hideMessage(modal) {
-    var mes = modal.getElementsByClassName("message")[0];
-    mes.classList.remove("error-message");
-    mes.classList.remove("success-message");
-    modal.getElementsByClassName("message")[0].style.visibility = "hidden";
+    var mes = getMessage(modal);
+    resetMessageStyle(mes);
+    mes.style.visibility = "hidden";
+}
+
+function resetMessageStyle(message) {
+    message.classList.remove(ERROR_MESSAGE_CLASS);
+    message.classList.remove(SUCCESS_MESSAGE_CLASS);
 }
 
 function showModal() {
